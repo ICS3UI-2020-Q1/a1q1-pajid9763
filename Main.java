f29892a5bcdee63cad8006adce209a4957e97584
pajid9763 <pajid9763@wrdsb.ca>
2020-09-13 20:07:40 +0000

    Finished program that converts Celsius to Fahrenheit

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program to convert Celsius to Fahrenheit 
+ * @author Darian Pajic
  */
 public class Main {
 
@@ -9,7 +10,18 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+  Scanner input = new Scanner(System.in);
+    // declare a variable that will store a name
+    int Celsius;
+
+    // ask the user what degrees in Celsius they want converted to Fahrenheit
+    System.out.println("Insert a degrees in Celsius that you want converted to Fahrenheit: ");
+    // initialize the name variable with Celsius
+    Celsius = input.nextInt();
+   
+    // declare and calculate their degrees Celsius into Fahrenheit
+    int Fahrenheit = (Celsius * 9)/5 + 32;
+    // tell the user their degrees in Fahrenheit
+    System.out.println(Celsius + "C " + "is equal to " + Fahrenheit + "F");
   }
 }
